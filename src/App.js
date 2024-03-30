f4f22c671f331fc80930b94141aecae4391b4804
Andy Liu <andycom12000@gmail.com>
2024-03-30 17:04:27 +0800

    加入測試用login功能

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import { RewardList } from './models/reward-list.js';
 import { useState } from 'react';
 import { useSearchParams } from 'react-router-dom';
+import { LoginModal } from './models/login.js';
 
 export default function App() {
   const [broadcasterInfo, setBroadcasterInfo] = useState(0);
@@ -24,18 +25,19 @@ export default function App() {
         <Navbar.Brand>TWPaws</Navbar.Brand>
         <Navbar.Toggle />
         <Navbar.Collapse className='justify-content-end'>
+          <LoginModal />
           {access_token ? 
-            <Navbar.Text>Signed in as: <a href="#home">{broadcasterInfo.display_name}</a></Navbar.Text> 
+            <Navbar.Text>Signed in as: <a href="#home">{broadcasterInfo.display_name}</a></Navbar.Text>
             : <Navbar.Text>
                 <a href="https://id.twitch.tv/oauth2/authorize?response_type=code&client_id=ecmnwqtzoa9c67bhtjunt7ne2vrog0&redirect_uri=https://www.twpaws.live/api/user/twitch_callback&scope=bits%3Aread+channel%3Amanage%3Amoderators+channel%3Amanage%3Aredemptions+channel%3Amanage%3Avips+channel%3Aread%3Aeditors+channel%3Aread%3Aredemptions+channel%3Aread%3Asubscriptions+channel%3Aread%3Avips+moderation%3Aread+moderator%3Amanage%3Aannouncements+moderator%3Amanage%3Aautomod+moderator%3Amanage%3Aautomod_settings+moderator%3Amanage%3Abanned_users+moderator%3Amanage%3Achat_messages+moderator%3Amanage%3Achat_settings+moderator%3Amanage%3Aguest_star+moderator%3Aread%3Aautomod_settings+moderator%3Aread%3Achat_settings+moderator%3Aread%3Achatters+moderator%3Aread%3Afollowers+moderator%3Aread%3Aguest_star+moderator%3Aread%3Ashield_mode+moderator%3Aread%3Ashoutouts+user%3Aedit+user%3Aedit%3Abroadcast+user%3Aedit%3Afollows+user%3Amanage%3Ablocked_users+user%3Amanage%3Achat_color+user%3Amanage%3Awhispers+user%3Aread%3Ablocked_users+user%3Aread%3Abroadcast+user%3Aread%3Aemail+user%3Aread%3Afollows+user%3Aread%3Asubscriptions+channel%3Abot+channel%3Amoderate+chat%3Aedit+chat%3Aread+user%3Abot+user%3Aread%3Achat">
-                  Login with Twitch
+                  Connect with Twitch
                 </a>
               </Navbar.Text>}
         </Navbar.Collapse>
       </Container>
     </Navbar>
     <div className="mainContainer">
-      {access_token ? <RewardList passBroadcasterInfo={getBroadcasterInfoFromChild} /> : <p>Please Login first</p>}
+      <RewardList passBroadcasterInfo={getBroadcasterInfoFromChild} />
     </div>
     </>
   );
---
+++ src/models/login.js
@@ -0,0 +1,76 @@
+import { Component } from "react";
+import { Button, Modal, Form, Row, FormGroup, Col } from "react-bootstrap";
+
+export class LoginModal extends Component {
+    URL_PREFIX = 'https://www.twpaws.live/api'
+    state = {
+        showModal: false,
+        isLoggingin: false
+    }
+
+    handleClose = () => this.setState({showModal: false})
+    handleShow = () => this.setState({showModal: true})
+
+    handleLogin = (e) => {
+        e.preventDefault();
+        this.setState({isLoggingin: true});
+        var requestBody = this.packLoginRequestBody(e.target);
+        fetch(this.URL_PREFIX.concat('/user/login'), {
+            method: "POST",
+            body: requestBody
+        })
+        .then((res) => res.json());
+    }
+
+    packLoginRequestBody = (data) => {
+        var formData = new FormData();
+        formData.append("username", data.username.value);
+        formData.append("password", data.password.value);
+        console.log(formData);
+        return formData;
+    }
+
+    render() {
+        return (
+            <>
+            <Button variant="primary" onClick={this.handleShow}>
+                Login
+            </Button>
+            <Modal show={this.state.showModal} onHide={this.handleClose}>
+                <Modal.Header closeButton>
+                    <Modal.Title>登入</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+                    <Form onSubmit={this.handleLogin} id="loginForm">
+                        <Row className="mb-2">
+                            <Form.Group as={Col} controlId="username">
+                                <Form.Label>使用者名稱</Form.Label>
+                                <Form.Control type="text" placeholder="使用者名稱"></Form.Control>
+                            </Form.Group>
+                        </Row>
+                        <Row className="mb-2">
+                            <Form.Group as={Col} controlId="password">
+                                <Form.Label>密碼</Form.Label>
+                                <Form.Control type="password" placeholder="密碼"></Form.Control>
+                            </Form.Group>
+                        </Row>
+                    </Form>
+                </Modal.Body>
+                <Modal.Footer>
+                    <Button variant="secondary" onClick={this.handleClose}>
+                        取消
+                    </Button>
+                    <Button 
+                        variant="primary"
+                        type="submit"
+                        form="loginForm"
+                        disabled={this.state.isLoggingin}
+                    >
+                        {this.state.isLoggingin ? "登入中..." : "登入"}
+                    </Button>
+                </Modal.Footer>
+            </Modal>
+            </>
+        )
+    }
+}
